047b7fd889f33d1259c9067dc2f36126d725b9b1
Anirudha <beleanirudha8@gmail.com>
2024-09-22 21:37:53 +0530

    create input box and button component using material ui

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,18 @@
+import { Box, Button, Grid, Grid2, TextField } from "@mui/material";
+
 export default function Home() {
-  return <h1>To-Do App</h1>
+  return (
+    <Box>
+      <Grid container spacing={2}>
+        <Grid item xs={6}>
+          <TextField fullWidth id="outlined-basic" label="Outlined" variant="outlined" />
+        </Grid>
+        <Grid item xs={6}>
+          <Button fullWidth variant="contained" sx={{
+            height: '100%'
+          }}>Add ToDo</Button>
+        </Grid>
+      </Grid>
+    </Box>
+  )
 }
